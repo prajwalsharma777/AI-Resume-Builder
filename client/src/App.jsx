8976b66752fd602419ae40cd42d57ff78a477ca7
Prajwal Sharma <prajwalsharma958@gmail.com>
2025-10-27 20:26:42 +0530

    Upload Files

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Preview from "./pages/Preview";
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard />}></Route>
+          <Route path="builder/:resumeId" element={<ResumeBuilder />}></Route>
+        </Route>
+
+        <Route path="view/:resumeId" element={<Preview />}></Route>
+        <Route path="view" element={<Login />}></Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
